dabda566643e2dbd48844220d1cbca461d95cef2
Vlad Muslimov <muslimov.vlad@mail.ru>
2023-09-09 00:23:38 +0500

    Initial commit

---
+++ src/test/java/Main.java
@@ -0,0 +1,27 @@
+import org.assertj.core.api.Assertions; // Импортируем необходимые классы из библиотек
+import org.junit.jupiter.api.Test;
+
+public class Main {
+    // Создаем класс MyTest, в котором будут размещены тесты
+    // Аннотация @Test указывает, что метод testAssertions() является тестовым методом
+    @Test
+    public void testAssertions() {
+        // Примеры использования AssertJ для проверки утверждений
+
+        // Создаем строку для тестирования
+        String text = "Hello, World";
+
+        // Проверяем, что строка не равна null
+        Assertions.assertThat(text).isNotNull();
+
+        // Проверяем, что строка начинается с "Hello"
+        Assertions.assertThat(text).startsWith("Hello");
+
+        // Проверяем, что строка заканчивается на "World"
+        Assertions.assertThat(text).endsWith("World");
+
+        // Проверяем, что строка содержит подстроку "lo, W"
+        Assertions.assertThat(text).contains("lo, W");
+    }
+}
+
